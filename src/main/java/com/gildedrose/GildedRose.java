099b8c5a931c1e0973237523270477cc8482f0fe
K Leonardo Ruiz Flórez <kleonardoruizflorez@gmail.com>
2023-06-05 14:30:19 +0200

    inlined one variable and reduced redundancy

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -15,20 +15,25 @@ class GildedRose {
             boolean isAgedBrie = item.name.equals("Aged Brie");
             boolean isBackstagePass = item.name.equals("Backstage passes to a TAFKAL80ETC concert");
 
-            item.sellIn = item.sellIn - 1;
-
-            if(isAgedBrie || isBackstagePass) {
+            if (isAgedBrie || isBackstagePass) {
                 if (item.quality < 50) {
                     increaseQualityBy1(item);
                 }
                 if (item.quality < 50 && isBackstagePass) {
-                    handleBackstageWithQualityLessThan50(item);
+                    if (item.sellIn < 11) {
+                        increaseQualityBy1(item);
+                    }
+                    if (item.sellIn < 6) {
+                        increaseQualityBy1(item);
+                    }
                 }
             } else {
                 if (item.quality > 0) {
                     decreaseQualityBy1(item);
                 }
             }
+
+            item.sellIn = item.sellIn - 1;
 
             if (item.sellIn < 0) {
                 if (!isAgedBrie) {
@@ -50,17 +55,6 @@ class GildedRose {
 
     private static void decreaseQualityBy1(Item item) {
         item.quality = item.quality - 1;
-    }
-
-    private static void handleBackstageWithQualityLessThan50(Item item) {
-        if (item.quality < 50) {
-            if (item.sellIn < 11) {
-                increaseQualityBy1(item);
-            }
-            if (item.sellIn < 6) {
-                increaseQualityBy1(item);
-            }
-        }
     }
 
     private static void increaseQualityBy1(Item item) {
