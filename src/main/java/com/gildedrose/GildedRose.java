e932751b326a10cac9f1e79f704f5baae44d66b6
K Leonardo Ruiz Flórez <kleonardoruizflorez@gmail.com>
2023-06-05 14:21:04 +0200

    added condition for sulfura items

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,13 +9,15 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
+            boolean isSulfuras = item.name.equals("Sulfuras, Hand of Ragnaros");
+            if (isSulfuras) continue;
+
             boolean isAgedBrie = item.name.equals("Aged Brie");
             boolean isBackstagePass = item.name.equals("Backstage passes to a TAFKAL80ETC concert");
-            boolean isSulfuras = item.name.equals("Sulfuras, Hand of Ragnaros");
 
             if (!isAgedBrie && !isBackstagePass) {
-                if (item.quality > 0 && !isSulfuras) {
-                        decreaseQualityBy1(item);
+                if (item.quality > 0) {
+                    decreaseQualityBy1(item);
                 }
             } else {
                 if (item.quality < 50) {
@@ -26,14 +28,12 @@ class GildedRose {
                 }
             }
 
-            if (!isSulfuras) {
-                item.sellIn = item.sellIn - 1;
-            }
+            item.sellIn = item.sellIn - 1;
 
             if (item.sellIn < 0) {
                 if (!isAgedBrie) {
                     if (!isBackstagePass) {
-                        if (item.quality > 0 && !isSulfuras) {
+                        if (item.quality > 0) {
                             decreaseQualityBy1(item);
                         }
                     } else {
